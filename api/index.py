359f00ea2e10478fe087650a72f2e11ad3dc280e
Купцова Юлия <kupcovaulia@MacBook-Air-Ula.local>
2025-08-12 16:45:48 +0900

    Update index.py with simple test endpoint

--- api/index.py
+++ api/index.py
@@ -1,21 +1,8 @@
-from fastapi import FastAPI, Request
-from telegram import Bot, Update
-import os
+from fastapi import FastAPI
 
 app = FastAPI()
-TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN)
 
-@app.post(f"/api/{TOKEN}")
-async def telegram_webhook(request: Request):
-    data = await request.json()
-    update = Update.de_json(data, bot)
-    
-    if update.message and update.message.text == "/start":
-        chat_id = update.message.chat.id
-        await bot.send_message(chat_id=chat_id, text="Привет! Я на Vercel!")
-
-    return {"ok": True}
-
-
+@app.get("/")
+async def root():
+    return {"message": "Server is running"}
 
